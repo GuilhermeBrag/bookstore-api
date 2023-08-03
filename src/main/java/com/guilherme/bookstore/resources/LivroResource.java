fb264c71cbe333a344d666cf0c3d9ccbfee0e3d6
GuilhermeBrag <guilhermebraga@gmail.com>
2023-08-03 18:07:43 -0300

    PUT e PATCH (ATUALIZAR) para Livro

--- src/main/java/com/guilherme/bookstore/resources/LivroResource.java
+++ src/main/java/com/guilherme/bookstore/resources/LivroResource.java
@@ -34,4 +34,18 @@ public class LivroResource {
        return ResponseEntity.ok().body(listDTO);
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj){
+        Livro newObj = livroService.update(id,obj);
+
+        return ResponseEntity.ok().body(newObj);
+    }
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
+        Livro newObj = livroService.update(id,obj);
+
+        return ResponseEntity.ok().body(newObj);
+    }
+
+
 }
